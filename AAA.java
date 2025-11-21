fe572b08f89d34f956aa1fe3612f58ce361f39cd
Vepusanan <vepusanan@gmail.com>
2025-11-21 11:38:41 +0530

    Practice

---
+++ AAA.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+// import java.util.Random;
+// import java.lang.Math;
+
+public class AAA {
+    public static void main(String[] args) {
+        /* Scanner object = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("Enter your age:");
+        int age = object.nextInt();
+        object.nextLine(); // Consume the newline character left by nextInt()
+        
+        System.out.print("Enter your name: ");
+        String name = object.nextLine();
+
+        System.out.println("Welcome " + name + " to Java Programming! and your age is " + age + " years.");
+
+        boolean luck = rand.nextBoolean();
+        System.out.println("Are you lucky?:" + luck); 
+        object.close(); */
+
+        double a = 9.99;
+        double b = 100.15;
+        double c = -54.01;
+
+        System.out.printf("The value of c is: %+.0f%n", b);
+
+        
+    }
+}   
